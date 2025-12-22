19c0d2d01f5d0f4b096aecfa7984d2969a7aed4f
Luis Eduardo Hernandez Morales <hdzluiseduardo089@gmail.com>
2025-12-22 16:12:51 -0600

    "Reto #00 realizado en Python"

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/LuisHdz089.py
@@ -0,0 +1,31 @@
+#https://www.python.org  (Forma oficial de un comentario en Python)
+'Este es un comentario de una sola línea en Python'
+"""
+Este es un comentario 
+de múltiples líneas en Python
+"""
+
+# Variables y tipos de datos en Python
+mi_variable = 10               # Entero
+mi_variable_flotante = 10.5    # Flotante   
+mi_variable_cadena = "Hola, Mundo!"  # Cadena de texto
+mi_variable_booleana = True     # Booleano
+mi_variable_lista = [1, 2, 3, 4, 5]  # Lista
+mi_variable_tupla = (1, 2, 3)   # Tupla
+mi_variable_diccionario = {'clave1': 'valor1', 'clave2': 'valor2'}  # Diccionario
+mi_variable_conjunto = {1, 2, 3}  # Conjunto
+mi_variable_nulo = None         # Nulo
+
+# Constantes en Python (por convención, se usan mayúsculas)
+MI_CONSTANTE = 10
+MI_CONSTANTE_FLOTANTE = 3.14
+MI_CONSTANTE_CADENA = "Constante de texto"  
+MI_CONSTANTE_BOLEANA = False
+MI_CONSTANTE_LISTA = [10, 20, 30]
+MI_CONSTANTE_TUPLA = (10, 20, 30)
+MI_CONSTANTE_DICCIONARIO = {'claveA': 'valorA', 'claveB': 'valorB'}
+MI_CONSTANTE_CONJUNTO = {10, 20, 30}
+MI_CONSTANTE_NULO = None
+
+# Imprimir "Hola, Python!" en la consola
+print("Hola, Python!")
